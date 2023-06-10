c0b94813a8a18e271683f55c7720d796c0151cc2
itsrazib1 <itsrazib1@gmail.com>
2023-06-10 17:53:02 +0600

    added deshbord part

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hmio3i.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -23,39 +20,57 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const classCollection = client.db("lanGuageDB").collection("Classes");
     const instactorCollection = client.db("lanGuageDB").collection("instactor");
+    const StudentclassCollection = client.db("lanGuageDB").collection("Studentclass");
 
     app.get("/Classes", async (req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
     });
-    
 
     app.get("/instactor", async (req, res) => {
       const result = await instactorCollection.find().toArray();
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
+    app.get("/Studentclass", async (req, res) => {
+      const result = await StudentclassCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/Studentclass', async (req, res) => {
+      const item = {
+        image: req.body.image,
+        name: req.body.name,
+        instructor: req.body.instructor,
+        availableSeats: req.body.availableSeats,
+        price: req.body.price,
+      };
+    
+      try {
+        const result = await StudentclassCollection.insertOne(item);
+        console.log("Inserted student class:", result);
+        res.status(200).json(result);
+      } catch (error) {
+        console.error("Error inserting student class:", error);
+        res.status(500).json({ error: "Failed to insert student class" });
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-//
-
 app.get("/", (req, res) => {
-  res.send("Sir is running");
+  res.send("Server is running");
 });
 
 app.listen(port, () => {
